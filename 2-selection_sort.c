38fecba523b11282c76b1051353e583a418f0692
Your Name <you@example.com>
2024-08-11 14:28:18 +0400

    added

---
+++ 2-selection_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+/**
+ * selection_sort - func
+ * @array: var
+ * @size: var
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0, min = 0;
+	int tmp = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+			if (*(array + j) < *(array + min))
+				min = j;
+		if (min != i)
+		{
+			tmp = *(array + min);
+			*(array + min) = *(array + i);
+			*(array + i) = tmp;
+		}
+		print_array(array, size);
+	}
+}
